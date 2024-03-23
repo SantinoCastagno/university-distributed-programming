2a7f6e5c53fee4078cda11475a937ebfc2c5c9ea
Santino Castagno <santinoo.rn@gmail.com>
2024-03-23 17:28:16 -0300

    tp1 init

---
+++ tp1/server_central.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define PORT 8080
+#define MAX_CONNECTIONS 10
+#define PORT_HOR_SERVER 9090
+#define PORT_WEA_SERVER 9091
+
+// function executed for each thread
+void *connection_handler(void *socket_desc)
+{
+    int client_socket = *(int *)socket_desc, status, client_fd, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    
+    // Socket creation to the other server
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        exit(EXIT_FAILURE);
+    }
+    // Configure server params
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT_HOR_SERVER);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        exit(EXIT_FAILURE);
+    }
+    // Stablish connection to the server
+    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr,
+                          sizeof(serv_addr))) < 0)
+    {
+        printf("\nConnection Failed \n");
+        exit(EXIT_FAILURE);
+    }
+    // Send message to the server
+    send(client_fd, hello, strlen(hello), 0);
+    printf("CS: Hello message sent to the HS\n");
+    // Read answer from the server 
+    valread = read(client_fd, buffer, 1024 - 1);
+    printf("CS: %s\n", buffer);
+
+    //send message to the client
+    write(client_socket, buffer, strlen(buffer));
+    
+    // close connection to the client
+    close(client_socket);
+    free(socket_desc);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    int server_fd, client_socket, *new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Socket creation
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(server_fd, MAX_CONNECTIONS) < 0)
+    {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Servidor escuchando en el puerto %d...\n", PORT);
+
+    while (1)
+    {
+        // Accept the connection instance
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Conexión aceptada\n");
+
+        // Create a new socket for the new client
+        new_socket = malloc(1);
+        *new_socket = client_socket;
+
+        // Create a new thread
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, connection_handler, (void *)new_socket) < 0)
+        {
+            perror("Thread creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Handler assigned\n");
+    }
+
+    return 0;
+}
