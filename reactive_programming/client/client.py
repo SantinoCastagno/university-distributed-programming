5a02b0568458a31e1c25dc0b57b1f009afd4e256
Santino Castagno <santinoo.rn@gmail.com>
2024-06-08 20:02:22 -0300

    feat: unidirectionally communication stablished

--- reactive_programming/client/client.py
+++ reactive_programming/client/client.py
@@ -5,27 +5,17 @@ import time
 import websockets
 from websockets import connect
 
-async def hello():
-    while True:
-        try:
-            async with connect("ws://localhost:8765") as ws:
-                while True:
-                    await ws.send("Hello world!")
-                    message = await ws.recv()
-                    print(f"Received: {message}")
-                    time.sleep(10)
-        except websockets.ConnectionClosed:
-            continue
 
 async def run_connection():
     async for websocket in connect("ws://localhost:8765"):
         try:
             while True:
-                        await websocket.send("Hello world!")
-                        message = await websocket.recv()
-                        print(f"Received: {message}")
-                        time.sleep(5)
+                await websocket.send("h")
+                message = await websocket.recv()
+                print(f"Received: {message}")
+                time.sleep(5)
         except websockets.ConnectionClosed:
+            time.sleep(5)
             print("LOG: connection failure, reconnecting..")
             continue
 
--- reactive_programming/server/server.py
+++ reactive_programming/server/server.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
-
 import asyncio
 from websockets.server import serve
+from reactivex import Subject
 
-ws_list = []
+msg_list = Subject()
+
+# def on_next(ws, msg_list):
+#     print("Hola")
+    
+def on_error(e):
+    print("Error Occurred: {0}".format(e))
+    
+def on_completed():
+    print("Clientes actualizados.")
 
 async def handler(ws):
-    ws_list.append(ws)
+    msg_list.subscribe(
+        on_next  = lambda i: print("Received {0}".format(i)),
+        on_error = on_error,
+        on_completed = on_completed,
+    )
     async for message in ws:
-        print(ws_list)
-        await ws.send(message)
+        msg_list.on_next(message)
+        
 
 async def main():
     async with serve(handler, "localhost", 8765):
